af9b03a89b5d0a2db95b00dd32dd2e9089f0a97e
WKHAllen <wkhallen@gmail.com>
2021-02-17 10:02:55 -0600

    Add NorseBooks error pages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import * as express from "express";
+import * as hbs from "express-handlebars";
 
 const port = parseInt(process.env.PORT);
 
 const app = express();
 
-app.use(express.static("static"));
+app.engine(
+  ".html",
+  hbs({
+    extname: ".html",
+  })
+);
+app.set("view engine", ".html");
+
+app.get("/:appName/:errorPage", (req, res) => {
+  const appName = req.params.appName;
+  const errorPage = req.params.errorPage;
+
+  res.render(`${appName}-${errorPage}`, { layout: appName });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
